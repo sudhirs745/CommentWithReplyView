d14c16b63fb46752dfac3a0f9511d0f4bbf39448
sudhir <sudhirs745@gmail.com>
2018-12-24 00:46:27 +0530

    Initial commit

---
+++ app/src/main/java/com/sud/commentwithreplylist/MainActivity.java
@@ -0,0 +1,288 @@
+package com.sud.commentwithreplylist;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.gson.Gson;
+import com.sud.commentwithreplylist.adapter.CommentExpandAdapter;
+import com.sud.commentwithreplylist.bean.CommentBean;
+import com.sud.commentwithreplylist.bean.CommentDetailBean;
+import com.sud.commentwithreplylist.bean.ReplyDetailBean;
+import com.sud.commentwithreplylist.view.CommentExpandableListView;
+
+import java.util.List;
+
+/**
+ * by sudhir sing  2018/12/12
+ * email:sudhirs745@gmail.com
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private TextView bt_comment;
+    private CommentExpandableListView expandableListView;
+    private CommentExpandAdapter adapter;
+    private CommentBean commentBean;
+    private List<CommentDetailBean> commentsList;
+    private BottomSheetDialog dialog;
+    private String testJson = "{\n" +
+            "\t\"code\": 1000,\n" +
+            "\t\"message\": \"View comments successfully\",\n" +
+            "\t\"data\": {\n" +
+            "\t\t\"total\": 3,\n" +
+            "\t\t\"list\": [{\n" +
+            "\t\t\t\t\"id\": 42,\n" +
+            "\t\t\t\t\"nickName\": \"Program\",\n" +
+            "\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\"content\": \"Time is the most precious wealth of all wealth。\",\n" +
+            "\t\t\t\t\"imgId\": \"4235\",\n" +
+            "\t\t\t\t\"replyTotal\": 1,\n" +
+            "\t\t\t\t\"createDate\": \"12.22\",\n" +
+            "\t\t\t\t\"replyList\": [{\n" +
+            "\t\t\t\t\t\"nickName\": \"sudhir\",\n" +
+            "\t\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\t\"id\": 40,\n" +
+            "\t\t\t\t\t\"commentId\": \"42\",\n" +
+            "\t\t\t\t\t\"content\": \"Time always passes by \",\n" +
+            "\t\t\t\t\t\"status\": \"01\",\n" +
+            "\t\t\t\t\t\"createDate\": \"fssgd\"\n" +
+            "\t\t\t\t}]\n" +
+            "\t\t\t},\n" +
+            "\t\t\t{\n" +
+            "\t\t\t\t\"id\": 41,\n" +
+            "\t\t\t\t\"nickName\": \"singh\",\n" +
+            "\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\"content\": \"this is ，hdrh！rhrtr。\",\n" +
+            "\t\t\t\t\"imgId\": \"xcclsscrt0tev11ok364\",\n" +
+            "\t\t\t\t\"replyTotal\": 1,\n" +
+            "\t\t\t\t\"createDate\": \"13.3pm\",\n" +
+            "\t\t\t\t\"replyList\": [{\n" +
+            "\t\t\t\t\t\"nickName\": \"sudhir\",\n" +
+            "\t\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\t\"commentId\": \"41\",\n" +
+            "\t\t\t\t\t\"content\": \"this is message .\",\n" +
+            "\t\t\t\t\t\"status\": \"01\",\n" +
+            "\t\t\t\t\t\"createDate\": \"5:54\"\n" +
+            "\t\t\t\t}]\n" +
+            "\t\t\t},\n" +
+            "\t\t\t{\n" +
+            "\t\t\t\t\"id\": 40,\n" +
+            "\t\t\t\t\"nickName\": \"amit\",\n" +
+            "\t\t\t\t\"userLogo\": \"http://ucardstorevideo.b0.upaiyun.com/userLogo/9fa13ec6-dddd-46cb-9df0-4bbb32d83fc1.png\",\n" +
+            "\t\t\t\t\"content\": \"2e325，353。\",\n" +
+            "\t\t\t\t\"imgId\": \"xcclsscrt0tev11ok364\",\n" +
+            "\t\t\t\t\"replyTotal\": 0,\n" +
+            "\t\t\t\t\"createDate\": \"5:24pm\",\n" +
+            "\t\t\t\t\"replyList\": []\n" +
+            "\t\t\t}\n" +
+            "\t\t]\n" +
+            "\t}\n" +
+            "}";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+        expandableListView = (CommentExpandableListView) findViewById(R.id.detail_page_lv_comment);
+        bt_comment = (TextView) findViewById(R.id.detail_page_do_comment);
+        bt_comment.setOnClickListener(this);
+
+        commentsList = generateTestData();
+        initExpandableListView(commentsList);
+    }
+
+    /**
+     * Initialize comments and reply lists
+     */
+    private void initExpandableListView(final List<CommentDetailBean> commentList){
+        expandableListView.setGroupIndicator(null);
+        //Expand all replies by default
+        adapter = new CommentExpandAdapter(this, commentList);
+        expandableListView.setAdapter(adapter);
+        for(int i = 0; i<commentList.size(); i++){
+            expandableListView.expandGroup(i);
+        }
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int groupPosition, long l) {
+                boolean isExpanded = expandableListView.isGroupExpanded(groupPosition);
+                Log.e(TAG, "onGroupClick: Current comment id>>>"+commentList.get(groupPosition).getId());
+//                if(isExpanded){
+//                    expandableListView.collapseGroup(groupPosition);
+//                }else {
+//                    expandableListView.expandGroup(groupPosition, true);
+//                }
+                showReplyDialog(groupPosition);
+                return true;
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int groupPosition, int childPosition, long l) {
+                Toast.makeText(MainActivity.this,"Clicked reply", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                //toast("Expand"+groupPosition+"Grouping");
+
+            }
+        });
+
+    }
+
+
+    private List<CommentDetailBean> generateTestData(){
+        Gson gson = new Gson();
+        commentBean = gson.fromJson(testJson, CommentBean.class);
+        List<CommentDetailBean> commentList = commentBean.getData().getList();
+        return commentList;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.detail_page_do_comment){
+
+            showCommentDialog();
+        }
+    }
+
+
+    private void showCommentDialog(){
+        dialog = new BottomSheetDialog(this);
+        View commentView = LayoutInflater.from(this).inflate(R.layout.comment_dialog_layout,null);
+        final EditText commentText = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button bt_comment = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        dialog.setContentView(commentView);
+        /**
+         * Solve the problem of incomplete  display
+         */
+        View parent = (View) commentView.getParent();
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(parent);
+        commentView.measure(0,0);
+        behavior.setPeekHeight(commentView.getMeasuredHeight());
+
+        bt_comment.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String commentContent = commentText.getText().toString().trim();
+                if(!TextUtils.isEmpty(commentContent)){
+
+                    //commentOnWork(commentContent);
+                    dialog.dismiss();
+                    CommentDetailBean detailBean = new CommentDetailBean("singh", commentContent,"12:34");
+                    adapter.addTheCommentData(detailBean);
+                    Toast.makeText(MainActivity.this,"Comment successful", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this,"Comment content cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        commentText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!TextUtils.isEmpty(charSequence) && charSequence.length()>2){
+                    bt_comment.setBackgroundColor(Color.parseColor("#FFB568"));
+                }else {
+                    bt_comment.setBackgroundColor(Color.parseColor("#D8D8D8"));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        dialog.show();
+    }
+
+
+    private void showReplyDialog(final int position){
+        dialog = new BottomSheetDialog(this);
+        View commentView = LayoutInflater.from(this).inflate(R.layout.comment_dialog_layout,null);
+        final EditText commentText = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button bt_comment = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        commentText.setHint("Reply " + commentsList.get(position).getNickName() + " Lovely:");
+        dialog.setContentView(commentView);
+        bt_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String replyContent = commentText.getText().toString().trim();
+                if(!TextUtils.isEmpty(replyContent)){
+
+                    dialog.dismiss();
+                    ReplyDetailBean detailBean = new ReplyDetailBean("sudhir",replyContent);
+                    adapter.addTheReplyData(detailBean, position);
+                    expandableListView.expandGroup(position);
+                    Toast.makeText(MainActivity.this,"Reply successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"Reply content cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        commentText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!TextUtils.isEmpty(charSequence) && charSequence.length()>2){
+                    bt_comment.setBackgroundColor(Color.parseColor("#FFB568"));
+                }else {
+                    bt_comment.setBackgroundColor(Color.parseColor("#D8D8D8"));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        dialog.show();
+    }
+
+}
---
+++ app/src/main/java/com/sud/commentwithreplylist/bean/CommentBean.java
@@ -0,0 +1,55 @@
+package com.sud.commentwithreplylist.bean;
+
+import java.util.List;
+
+/**
+ * by sudhir sing  2018/12/12
+ * email:sudhirs745@gmail.com
+ */
+
+public class CommentBean {
+    private int code;
+    private String message;
+    private Data data;
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public int getCode() {
+        return code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+    public Data getData() {
+        return data;
+    }
+
+    public class Data {
+
+        private int total;
+        private List<CommentDetailBean> list;
+        public void setTotal(int total) {
+            this.total = total;
+        }
+        public int getTotal() {
+            return total;
+        }
+
+        public void setList(List<CommentDetailBean> list) {
+            this.list = list;
+        }
+        public List<CommentDetailBean> getList() {
+            return list;
+        }
+
+    }
+
+}
